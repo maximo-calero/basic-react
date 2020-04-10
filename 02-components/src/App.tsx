e3f8d05017a7e6580b055457c00a3b036e1a020d
Maximo Calero Sanchez <maximo.calero@tokiota.com>
2020-04-10 12:47:40 +0200

    02-01 Componentes

--- 02-components/src/App.tsx
+++ 02-components/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import defaultImage from './images/default-image_300.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Buscador de películas</h1>
+        <div>
+          <input type='text' value='' placeholder='Introduzca el nombre de una película'/>
+          <button value='Buscar'>Buscar</button>
+        </div>
+        <div>
+          <h2>Resultados búsqueda</h2>
+          <div>
+            <div>
+              <img src={defaultImage} />
+              <span>Titulo película</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
