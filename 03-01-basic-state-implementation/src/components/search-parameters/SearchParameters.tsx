f9a6322d6cf2c6e35f60a447a1ffdc88818a04b3
Maximo Calero Sanchez <maximo.calero@tokiota.com>
2020-04-12 13:03:22 +0200

    03-01 basic state management implementation

--- 03-01-basic-state-implementation/src/components/search-parameters/SearchParameters.tsx
+++ 03-01-basic-state-implementation/src/components/search-parameters/SearchParameters.tsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './SearchParameters.css';
 import searchIcon from '../../images/search-icon.png';
 
-const SearchParameters = () => {
+interface Props {
+  queryText: string;
+  onChangeQueryText: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const SearchParameters = (props: Props) => {
     return (
         <div className='search-parameters-container'>
-          <input className='search-text-box' type='text' value='' placeholder='Introduzca el nombre de una película'/>
-          <button className='search-button' value='Buscar'>
+          <input onChange={props.onChangeQueryText} className='search-text-box' type='text' value={props.queryText} placeholder='Introduzca el nombre de una película'/>
+          <button className='search-button' value='Buscar'> 
             <img alt='search icon' className='search-icon' src={searchIcon} />            
           </button>
         </div>        
--- 03-01-basic-state-implementation/src/pages/home/Home.tsx
+++ 03-01-basic-state-implementation/src/pages/home/Home.tsx
@@ -4,68 +4,41 @@ import SearchParameters from '../../components/search-parameters/SearchParameter
 import ResultSearch from '../../components/result-search/ResultSearch';
 import { Movie } from '../../common/interfaces/movie';
 
-class App extends React.Component {
-  movies: Movie[] = [
-    {
-      id: '1',
-      imageUrl: '',
-      title: 'Movie 01'
-    },
-    {
-      id: '2',
-      imageUrl: '',
-      title: 'Movie 02'
-    },
-    {
-      id: '3',
-      imageUrl: '',
-      title: 'Movie 03'
-    },
-    {
-      id: '4',
-      imageUrl: '',
-      title: 'Movie 04'
-    },
-    {
-      id: '5',
-      imageUrl: '',
-      title: 'Movie 05'
-    },
-    {
-      id: '6',
-      imageUrl: '',
-      title: 'Movie 06'
-    },
-    {
-      id: '7',
-      imageUrl: '',
-      title: 'Movie 07'
-    },
-    {
-      id: '8',
-      imageUrl: '',
-      title: 'Movie 08'
-    },
-    {
-      id: '9',
-      imageUrl: '',
-      title: 'Movie 09'
-    },
-    {
-      id: '10',
-      imageUrl: '',
-      title: 'Movie 10'
-    },
-  ];
+export interface Props {
+
+}
+
+export interface State {
+  queryText: string;
+  movies: Movie[];
+}
+
+class Home extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      queryText: '',
+      movies: []
+    };
+  }
+
+  handleQueryText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const target = event.target;
+    this.setState(prevState => ({
+      ...prevState,
+      queryText: target.value
+    }));
+  }
+
   render() {
     return (
       <div className="main-container">
         <h1 className='title'>Buscador de películas</h1>
-        <SearchParameters />
-        <ResultSearch movies={this.movies} />
+        <SearchParameters onChangeQueryText={this.handleQueryText} queryText={this.state.queryText} />
+        <ResultSearch movies={this.state.movies} />
       </div>
     );
   }
 }
 
-export default App;
+export default Home;
