438b4b2b1ead958746c57fd717a3c265e079bfbc
Maximo Calero Sanchez <maximo.calero@tokiota.com>
2020-04-12 10:18:13 +0200

    02-03 Refactoring

---
+++ 04-refactoring/src/components/result-search/ResultSearch.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './ResultSearch.css';
+import { Movie } from '../../common/interfaces/movie';
+import defaultImage from '../../images/default-image_300.png';
+
+interface Props {
+    movies: Movie[];
+}
+
+const ResultSearch = (props: Props) => {
+    return (
+        <div className='result-search'>
+            <div className='result-items'>
+            {props.movies && props.movies.length > 0 &&
+                props.movies.map(movie => {
+                    return (
+                    <div key={movie.id} className='result-item'>
+                        <img src={movie.imageUrl ? movie.imageUrl : defaultImage} />
+                        <span>{movie.title}</span>
+                    </div>
+                    );
+                })
+            }
+            {props.movies && props.movies.length === 0 &&
+            (
+                <div className='no-results-message'>
+                    <span>La búsqueda no devolvió resultados</span>
+                </div>  
+            )
+            }
+            </div>
+        </div>        
+    );
+}
+
+export default ResultSearch;
---
+++ 04-refactoring/src/components/search-parameters/SearchParameters.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SearchParameters.css';
+import searchIcon from '../../images/search-icon.png';
+
+const SearchParameters = () => {
+    return (
+        <div className='search-parameters-container'>
+          <input className='search-text-box' type='text' value='' placeholder='Introduzca el nombre de una película'/>
+          <button className='search-button' value='Buscar'>
+            <img className='search-icon' src={searchIcon} />            
+          </button>
+        </div>        
+    );
+}
+
+export default SearchParameters;
