b794fba005ebb67b16a83dffd7d72e4f2c03fa64
Maximo Calero Sanchez <maximo.calero@tokiota.com>
2020-04-13 11:56:09 +0200

    API call implemented

--- 03-02-button-handler/src/pages/home/Home.tsx
+++ 03-02-button-handler/src/pages/home/Home.tsx
@@ -3,6 +3,8 @@ import './Home.css';
 import SearchParameters from '../../components/search-parameters/SearchParameters';
 import ResultSearch from '../../components/result-search/ResultSearch';
 import { Movie } from '../../common/interfaces/movie';
+import { dataService } from '../../common/service/dataservice';
+import { SearchResults } from '../../common/interfaces/searchresults';
 
 export interface Props {
 
@@ -30,66 +32,17 @@ class Home extends React.Component<Props, State> {
     }));
   }
 
-  handleSearchClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+  handleSearchClick = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
-    const movies: Movie[] = [
-          {
-            id: '1',
-            imageUrl: '',
-            title: 'Movie 01'
-          },
-          {
-            id: '2',
-            imageUrl: '',
-            title: 'Movie 02'
-          },
-          {
-            id: '3',
-            imageUrl: '',
-            title: 'Movie 03'
-          },
-          {
-            id: '4',
-            imageUrl: '',
-            title: 'Movie 04'
-          },
-          {
-            id: '5',
-            imageUrl: '',
-            title: 'Movie 05'
-          },
-          {
-            id: '6',
-            imageUrl: '',
-            title: 'Movie 06'
-          },
-          {
-            id: '7',
-            imageUrl: '',
-            title: 'Movie 07'
-          },
-          {
-            id: '8',
-            imageUrl: '',
-            title: 'Movie 08'
-          },
-          {
-            id: '9',
-            imageUrl: '',
-            title: 'Movie 09'
-          },
-          {
-            id: '10',
-            imageUrl: '',
-            title: 'Movie 10'
-          },
-    ];
-    this.setState(prevState => (
-      {
-        ...prevState,
-        movies: movies
-      }
-    ));
+    if (this.state.queryText) {
+      const searchResults: SearchResults = await dataService.searchMovies(this.state.queryText, 1);
+      this.setState(prevState => (
+        {
+          ...prevState,
+          movies: searchResults.results
+        }
+      ));  
+    }
   }
 
   render() {
